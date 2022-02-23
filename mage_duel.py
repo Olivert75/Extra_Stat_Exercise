f93faa1a78b4e0a19ab1d7d20fd2c15e08e99576
Olivert75 <oliver.ton75@gmai.com>
2022-02-23 14:47:39 -0600

    Create data frame for the mage

--- mage_duel.py
+++ mage_duel.py
@@ -1,8 +1,7 @@
 import numpy as np
 import pandas as pd
 
-wiz_df = pd.DataFrame()
-
+#Create stats for wizard 1, 2
 def Wiz_stats():
     def Wiz_1_hp():
         Wiz_One_HP = np.random.choice([1,2,3,4], size= 6)
@@ -15,7 +14,17 @@ def Wiz_stats():
         return Wiz_Two_HP.sum()
     def Wiz_2_damage():
         return Wiz_2_hp()
+
     return Wiz_1_hp(), Wiz_1_damage(), Wiz_2_hp(), Wiz_2_damage()
 
-Wiz_stats()
+#Assign variables
 Wiz_1_HP, Wiz_1_Attack, Wiz_2_HP, Wiz_2_Attack = Wiz_stats()
+
+#Create a dataframe 
+wiz_df = pd.DataFrame()
+wiz_df['wiz_1_hp'] = Wiz_1_HP
+wiz_df['wiz_1_att'] = Wiz_1_Attack
+wiz_df['wiz_2_hp'] = Wiz_2_HP
+wiz_df['wiz_2_att'] = Wiz_2_Attack
+
+
