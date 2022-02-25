4bde34a1e552607462f0c84c79211dd610df1a48
Olivert75 <oliver.ton75@gmai.com>
2022-02-24 18:52:22 -0600

    Broke down the problem and then write it in a function later

--- mage_duel.py
+++ mage_duel.py
@@ -2,32 +2,25 @@ import numpy as np
 import pandas as pd
 
 #Create stats for wizard 1, 2
-def Wiz_stats():
-    def Wiz_1_hp():
-        Wiz_One_HP = np.random.choice([1,2,3,4], size= 6)
-        return Wiz_One_HP.sum()
-    def Wiz_1_damage():
-        return Wiz_1_hp()
-        
-    def Wiz_2_hp():
-        Wiz_Two_HP = np.random.choice([1,2,3,4,5,6], size= 4)
-        return Wiz_Two_HP.sum()
-    def Wiz_2_damage():
-        return Wiz_2_hp()
+Wiz_One_HP = np.random.choice([1,2,3,4], size= 6)
+Wiz_1_damage = np.random.choice([1,2,3,4], size= 6)
 
-    return Wiz_1_hp(), Wiz_1_damage(), Wiz_2_hp(), Wiz_2_damage()
+Wiz_Two_HP = np.random.choice([1,2,3,4,5,6], size= 4)
+Wiz_2_damage = np.random.choice([1,2,3,4,5,6], size= 4)
 
+Wiz_One_HP
+Wiz_1_damage
 #Assign variables
-Wiz_1_HP, Wiz_1_Attack, Wiz_2_HP, Wiz_2_Attack = Wiz_stats()
+#Wiz_1_HP, Wiz_1_Attack, Wiz_2_HP, Wiz_2_Attack = Wiz_stats()
 
 #Create a dataframe 
-wiz_df = pd.DataFrame(Wiz_stats())
-wiz_df['wiz_1_hp'] = Wiz_1_HP
-wiz_df['wiz_1_att'] = Wiz_1_Attack
-wiz_df['wiz_2_hp'] = Wiz_2_HP
-wiz_df['wiz_2_att'] = Wiz_2_Attack
+wiz_df = pd.DataFrame()
+wiz_df['wiz_1_hp'] = Wiz_One_HP.sum()
+wiz_df['wiz_1_att'] = Wiz_1_damage.sum()
+wiz_df['wiz_2_hp'] = Wiz_Two_HP.sum()
+wiz_df['wiz_2_att'] = Wiz_2_damage.sum()
 
-wiz_df.wiz_1_hp
+wiz_df
 
 #Create simulation battle
 wiz_df['wins']= (wiz_df.wiz_1_hp > wiz_df.wiz_2_att) & (wiz_df.wiz_2_hp <= wiz_df.wiz_1_att)
